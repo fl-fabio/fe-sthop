4fcf685061f9f65b8b69d19f879bceafbee4e296
fl-fabio <fl.fabio.lanzafame@gmail.com>
2024-02-23 10:34:18 +0100

    feature(): added navbar counter

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Product } from "./types/Product";
 import "./App.css";
 import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 
-  const [products, setProducts] = useState<Product[]>([])
-  const cards = Array.from({ length: 20 });
+  const [products, setProducts] = useState<Product[]>([]);
+  const [totQuantity, setTotQuantity] = useState<number>(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,20 +16,28 @@ function App() {
       );
       const data = await response.json();
       console.log(data);
-      setProducts(data)
+      setProducts([...data].map(product => ({...product, quantity: 0})));
     }
 
     fetchData();
   },[]);
 
+  useEffect(() => {
+    setTotQuantity(products.reduce((acc, product) => acc + product.quantity, 0))
+  },[products])
+
+  console.log(products)
 
   return (
     <div className="container-fluid">
+      <Navbar totQuantity={totQuantity}/>
       <h1 className="text-center">My Shop</h1>
       <div className="row">
-        {products.map((product, index) => (
-          <Card item={product} index={index}/>
-        ))}
+        {products.map((product, index) => {
+          return (
+              <Card item={product} index={index} setProducts={setProducts}/>
+            )
+        })}
       </div>
     </div>
   );
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,25 +1,39 @@
-import React from "react";
-import './Card.css';
+import React, { useEffect, useState } from "react";
+import "./Card.css";
 import { Product } from "../../types/Product";
 
 interface CardProps {
-    item: Product,
-    index: number
+  item: Product;
+  index: number;
+  setProducts: Function;
 }
-const Card = ({item, index}: CardProps) => {
+const Card = ({ item, index, setProducts}: CardProps) => {
+  const [quantity, setQuantity] = useState<number>(0);
+
+useEffect(() => {
+    setProducts((prev: Product[]) => {
+      const updateProduct = [...prev];
+      updateProduct[index] = {...prev[index], quantity: quantity};
+      console.log(quantity)
+      return updateProduct;
+    });
+  }, [index, quantity, setProducts]) 
+
+  const changeQuantity = (quantity: number) => {
+    setQuantity(prev => Math.max(0, prev + quantity));
+  }
 
   return (
     <div className="card col-4" style={{ width: "18rem" }}>
-      <img
-        src={item.image}
-        className="card-img-top"
-        alt={"card"}
-      />
+      <img src={item.image} className="card-img-top" alt={"card"} />
       <div className="card-body">
         <h5 className="card-title truncate">{item.title}</h5>
-        <p className="card-text truncate">
-          {item.description}
-        </p>
+        <p className="card-text truncate">{item.description}</p>
+        <div className="prod-quantity d-flex justify-content-center my-2">
+          <button className="btn btn-danger" onClick={() => changeQuantity(-1)}>-</button>
+          <div className="mx-2">{quantity}</div>
+          <button className="btn btn-primary" onClick={() => changeQuantity(+1)}>+</button>
+        </div>
         <p className="price">{`${item.price} €`}</p>
       </div>
     </div>
